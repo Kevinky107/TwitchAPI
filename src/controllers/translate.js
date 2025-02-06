f7485243d09637e0998d4db6452f061294286fbd
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2025-02-06 11:59:07 +0100

    FIX: change the way the translation is shown after the calll
    This wasy is easier to make it compatible with NightBot in Twitch

--- src/controllers/translate.js
+++ src/controllers/translate.js
@@ -10,7 +10,7 @@ translateController.get('/:q', async (_req, res, _next) => {
     const q = _req.params.q;
     try {
         const result = await translator.translateText(q, null, 'EN-US');
-        return res.status(200).json(result);
+        return res.status(200).send(result.text);
     } catch (error) {
         console.error('Error en la traducción:', error);
         return res.status(500).json({ error: error.message || 'Error en la traducción' });
@@ -21,7 +21,7 @@ translateController.get('/es-en/:q', async (_req, res, _next) => {
     const q = _req.params.q;
     try {
         const result = await translator.translateText(q, 'ES', 'EN-US');
-        return res.status(200).json(result);
+        return res.status(200).send(result.text);
     } catch (error) {
         console.error('Error en la traducción:', error);
         return res.status(500).json({ error: error.message || 'Error en la traducción' });
@@ -31,8 +31,8 @@ translateController.get('/es-en/:q', async (_req, res, _next) => {
 translateController.get('/en-es/:q', async (_req, res, _next) => {
     const q = _req.params.q;
     try {
-        const result = await translator.translateText(q, 'EN-US', 'ES');
-        return res.status(200).json(result);
+        const result = await translator.translateText(q, null, 'ES');
+        return res.status(200).send(result.text);
     } catch (error) {
         console.error('Error en la traducción:', error);
         return res.status(500).json({ error: error.message || 'Error en la traducción' });
